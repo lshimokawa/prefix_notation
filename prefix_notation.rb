2a4b2f2e32c256c9ad1429d6a59c8a44f0b27451
Lennon Shimokawa <lennon@devacademy.la>
2017-01-11 16:04:40 -0500

    Refactor each in one line

--- prefix_notation.rb
+++ prefix_notation.rb
@@ -2,9 +2,7 @@ class PrefixNotation
 
   def evaluate(expression)
     stack = []
-    expression.split(' ').reverse.each do |element|
-      (['+', '-', '*', '/'].include? element) ? stack.push(stack.pop.send(element, stack.pop)) : stack.push(element.to_i)
-    end
+    expression.split(' ').reverse.each { |element| (['+', '-', '*', '/'].include? element) ? stack.push(stack.pop.send(element, stack.pop)) : stack.push(element.to_i) }
     stack.first
   end
 
