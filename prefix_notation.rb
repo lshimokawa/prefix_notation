ec92c197d30c1bf71cee71d5e5da170d3fdabeca
Lennon Shimokawa <lennon@devacademy.la>
2017-01-11 14:06:51 -0500

    Adds 2 numbers

--- prefix_notation.rb
+++ prefix_notation.rb
@@ -1,6 +1,11 @@
 class PrefixNotation
 
   def evaluate(expression)
-    expression.to_i
+    array = expression.split(' ')
+    if array.size == 1
+      result = array[0].to_i    
+    else
+      result = array[1].to_i + array[2].to_i
+    end
   end
 end
--- spec/prefix_notation_spec.rb
+++ spec/prefix_notation_spec.rb
@@ -1,9 +1,15 @@
 require_relative '../prefix_notation'
 
 RSpec.describe PrefixNotation do
-  it "evaluates a positive integer" do
-    p = PrefixNotation.new
-    result = p.evaluate("9")
-    expect(result).to eq 9
+
+  subject(:pn) { PrefixNotation.new }
+
+  it 'evaluates a positive integer' do
+    expect(pn.evaluate('9')).to eq 9
   end
+
+  it "adds 2 numbers" do
+    expect(pn.evaluate('+ 1 2')).to eq 3
+  end
+
 end
