f36cbfa27f93847cb18b68f282bd90bf52317b25
Lennon Shimokawa <lennon@devacademy.la>
2017-01-11 22:34:42 -0500

    Refactor using reduce

--- prefix_notation.rb
+++ prefix_notation.rb
@@ -1,9 +1,7 @@
 class PrefixNotation
 
   def evaluate(expression)
-    stack = []
-    expression.split.reverse.each { |element| stack.push((['+', '-', '*', '/'].include? element) ? stack.pop.send(element, stack.pop) : element.to_i) }
-    stack.first
+    expression.split.reverse.reduce([]) { |stack, element| stack.push((['+', '-', '*', '/'].include? element) ? stack.pop.send(element, stack.pop) : element.to_i) }.pop
   end
 
 end
