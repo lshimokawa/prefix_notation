93fd6c1eb28070ce64f830495eec2cf3b01b97db
Lennon Shimokawa <lennon@devacademy.la>
2017-01-11 22:32:21 -0500

    Refactor ternary operator

--- prefix_notation.rb
+++ prefix_notation.rb
@@ -2,7 +2,7 @@ class PrefixNotation
 
   def evaluate(expression)
     stack = []
-    expression.split(' ').reverse.each { |element| (['+', '-', '*', '/'].include? element) ? stack.push(stack.pop.send(element, stack.pop)) : stack.push(element.to_i) }
+    expression.split(' ').reverse.each { |element| stack.push((['+', '-', '*', '/'].include? element) ? stack.pop.send(element, stack.pop) : element.to_i) }
     stack.first
   end
 
