0dff297286b11e66b2e992eb25accdfae9d9c257
Lennon Shimokawa <lennon@devacademy.la>
2017-01-11 22:32:46 -0500

    Refactor split default

--- prefix_notation.rb
+++ prefix_notation.rb
@@ -2,7 +2,7 @@ class PrefixNotation
 
   def evaluate(expression)
     stack = []
-    expression.split(' ').reverse.each { |element| stack.push((['+', '-', '*', '/'].include? element) ? stack.pop.send(element, stack.pop) : element.to_i) }
+    expression.split.reverse.each { |element| stack.push((['+', '-', '*', '/'].include? element) ? stack.pop.send(element, stack.pop) : element.to_i) }
     stack.first
   end
 
